9838de5edf8e084b8455dd6943954b9551774487
Aarick-F <zeflyndesign@gmail.com>
2018-03-29 18:25:54 -0400

    added logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,5 +13,45 @@ $(document).ready(() => {
 
   var database = firebase.database();
 
-  console.log(database);
+  // Variables
+  let name, destination, firstTime, frequency, timeDifference, minutesLeft, nextArrival;
+
+  $("#addTrain").on("click", function() {
+    name = $("#trainName").val().trim();
+    destination = $("#trainDestination").val().trim();
+    firstTime = $("#trainTime").val().trim();
+    frequency = $("#trainFrequency").val().trim();
+
+    // Takes input first time, subtracts it from the current time provided by moment
+    timeDifference = moment().diff(moment(firstTime, "HHmm"), "minutes");
+    leftOverTime = timeDifference % frequency;
+    minutesLeft = frequency - leftOverTime;
+    nextArrival = moment().add(minutesLeft, "minutes").format("h:mm A");
+
+    // Push info to database
+    database.ref().push({
+      name: name,
+      destination: destination,
+      firstTime: firstTime,
+      frequency: frequency,
+      nextArrival: nextArrival,
+      minutesLeft: minutesLeft
+    });
+
+    $("#trainName").val("");
+    $("#trainDestination").val("");
+    $("#trainTime").val("");
+    $("#trainFrequency").val("");
+    console.log(minutesLeft);
+  });
+
+  // When data is pushed into our database:
+  database.ref().on("child_added", function(snapshot) {
+    let newRow = $("<ul class='listRow'><li>" + snapshot.val().name + "</li>" +
+                  "<li>" + snapshot.val().destination + "</li>" +
+                  "<li>" + snapshot.val().frequency + "</li>" +
+                  "<li>" + snapshot.val().nextArrival + "</li>" +
+                  "<li>" + snapshot.val().minutesLeft + "</li></ul>");
+    $("#display").append(newRow);
+  });
 });
